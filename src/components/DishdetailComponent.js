08f0068d502f8990504cfd84401f63a7d59e7924
Jamal Rizki <jamalbrizki@gmail.com>
2021-01-28 18:14:19 -0500

    new

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -17,6 +17,7 @@ import {
 } from "reactstrap";
 import { Control, LocalForm, Errors } from "react-redux-form";
 import { Link } from 'react-router-dom';
+import { Loading } from './LoadingComponent';
 
 const required = val => val && val.length;
 const maxLength = len => val => !val || val.length <= len;
@@ -128,7 +129,6 @@ class CommentForm extends Component {
 
 
 function RenderDish({ dish }) {
-  if (dish != null) {
     return (
       <div className="col-12 col-md-5 m-1">
         <Card>
@@ -140,12 +140,12 @@ function RenderDish({ dish }) {
         </Card>
       </div>
     );
-  } else {
-    return (
-      <div></div>
-    );
   }
-}
+    
+
+
+  
+
 
 
 function RenderComments({comments, addComment, dishId}) {
@@ -184,7 +184,25 @@ function RenderComments({comments, addComment, dishId}) {
 }
 
 const DishDetail = (props) => {
-  if (props.dish != null) {
+  if (props.isLoading) {
+    return(
+        <div className="container">
+            <div className="row">            
+                <Loading />
+            </div>
+        </div>
+    );
+}
+else if (props.errMess) {
+    return(
+        <div className="container">
+            <div className="row">            
+                <h4>{props.errMess}</h4>
+            </div>
+        </div>
+    );
+}
+ else if (props.dish != null) {
     return (
       <div className="container">
         <div className="row">
